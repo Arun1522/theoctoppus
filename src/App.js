690bd09644424d90a2f6056a25403d8fdb392b23
Arun1522 <pandeyarun602@gmail.com>
2024-05-18 12:33:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <>
+      <h2 style={{ textAlign: "center" }}>
+        Click a company below to track your courier.
+      </h2>
+      <div className="button-container">
+        <img
+          src="https://trackon.in/images/logo.svg"
+          alt="Trackon Logo"
+          className="company-logo"
+          style={{ width: "172px" }}
+        />
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://trackon.in/Tracking/t2/MultipleTracking"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <button className="custom-button" style={{ background: "#f45c09" }}>
+            Click Here
+          </button>
         </a>
-      </header>
-    </div>
+        <img
+          src="https://www.delhivery.com/_nuxt/img/Delhivery_logo.dcef706.png"
+          alt="Trackon Logo"
+          className="company-logo"
+          style={{ background: "#101218", width: "172px", height: "56px" }}
+        />
+        <a
+          href="https://www.delhivery.com/tracking"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="custom-button" style={{ background: "#000" }}>
+            Click Here
+          </button>
+        </a>
+        <img
+          src="https://www.dtdc.in/img/logos/logo-footer.png"
+          alt="Trackon Logo"
+          className="company-logo"
+          style={{ background: "#101218", width: "172px", height: "56px" }}
+        />
+        <a
+          href="https://www.dtdc.in/tracking.asp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button
+            className="custom-button"
+            style={{ background: "#fff", color: "#000" }}
+          >
+            Click Here
+          </button>
+        </a>
+        <img
+          src="https://www.bluedart.com/image/layout_set_logo?img_id=1414225&t=1715615127373"
+          alt="Trackon Logo"
+          className="company-logo"
+          style={{ width: "172px", height: "56px" }}
+        />
+        <a
+          href="https://bluedart.com/tracking"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="custom-button">Click Here</button>
+        </a>
+      </div>
+    </>
   );
 }
 
